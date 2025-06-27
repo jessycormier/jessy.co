f317438674a53e4d9fd91c33d9216c56578d234a
Jessy Cormier <jessy@jessy.co>
2025-06-27 14:52:55 -0300

    theme service, lucide icons

--- src/app/shared/components/back-to-home-button/back-to-home-button.component.ts
+++ src/app/shared/components/back-to-home-button/back-to-home-button.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IconsModule } from '../../icon.module';
 
 @Component({
   selector: 'app-back-to-home-button',
-  imports: [RouterLink],
+  imports: [RouterLink, IconsModule],
   templateUrl: './back-to-home-button.component.html',
 })
 export class BackToHomeButtonComponent {}
--- src/app/shared/components/mask/mask.component.ts
+++ src/app/shared/components/mask/mask.component.ts
@@ -1,5 +1,5 @@
 import { Component, effect } from '@angular/core';
-import { LayoutService } from '../../../services/layout.service';
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
   selector: 'app-mask',
